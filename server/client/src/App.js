87dbc18273f188d62739d2ce25b684fde1b0798b
Karan Dhanoya <kdhanoya94@gmail.com>
2018-12-12 18:04:48 +0000

    restart

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import FitnessForm from './components/FitnessForm'
+
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <FitnessForm />
+        app
       </div>
     );
   }
